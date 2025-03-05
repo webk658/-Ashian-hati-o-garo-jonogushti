d4f5b594fff88ad3ea8936c0d2d159b19ba9f328
Megong <80638606+webk658@users.noreply.github.com>
2025-03-05 13:29:44 +0600

    Js file
    This is my web story.

---
+++ app.js
@@ -0,0 +1,12 @@
+// চেক করুন যদি ব্রাউজার Service Worker সমর্থন করে
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js')
+            .then(registration => {
+                console.log('Service Worker registered with scope: ', registration.scope);
+            })
+            .catch(error => {
+                console.log('Service Worker registration failed: ', error);
+            });
+    });
+}
